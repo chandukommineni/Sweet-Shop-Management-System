8c42575359110e21083357cbd048262b24461a1c
Chandu Kommineni <chandukommineni63@gmail.com>
2025-09-19 22:15:07 +0530

    - Folder are Created for frontend and Backend - Backend Project is Created and Database configuration provided

---
+++ backend/SweetShopManagementSystem/src/test/java/com/sweetshop/management/system/SweetShopManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sweetshop.management.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetShopManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
